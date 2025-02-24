dc02e689ebd3a5eec28e545be63c4093c55fba0f
Nicolas Amadori <nicola.amadori@studio.unibo.it>
2025-02-24 10:11:46 +0100

    testInitialState success

---
+++ tdd-exercise/src/main/java/tdd/SmartDoorLockImpl.java
@@ -0,0 +1,49 @@
+package tdd;
+
+public class SmartDoorLockImpl implements SmartDoorLock{
+
+    final int maxAttempts;
+
+    public SmartDoorLockImpl(final int maxAttempts) {
+        this.maxAttempts = maxAttempts;
+    }
+    @Override
+    public void setPin(int pin) {
+
+    }
+
+    @Override
+    public void unlock(int pin) {
+
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public boolean isLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return false;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return 0;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,14 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testInitialState() {
+        SmartDoorLockImpl lock = new SmartDoorLockImpl(5);
+        assertTrue(lock.isLocked());
     }
 }
