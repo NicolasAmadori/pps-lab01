43959257f45fb97bcb1b0d10b0f89233f0d84af9
Nicolas Amadori <nicola.amadori@studio.unibo.it>
2025-02-24 10:12:56 +0100

    testInitialState refactor

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -6,9 +6,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
+
+    public static final int MAX_ATTEMPTS = 5;
+    SmartDoorLockImpl lock;
+
+    @BeforeEach
+    public void test(){
+        lock = new SmartDoorLockImpl(MAX_ATTEMPTS);
+    }
+
     @Test
     public void testInitialState() {
-        SmartDoorLockImpl lock = new SmartDoorLockImpl(5);
         assertTrue(lock.isLocked());
     }
 }
